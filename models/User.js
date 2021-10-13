c94f475228a1868068122c6dc843082985f4a8f1
legalvao1 <legalvao1@gmail.com>
2021-10-13 15:37:46 -0300

    Configurando model de User e a migration

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const User = (sequelize, DataTypes) => {
+  const user = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+
+  return user;
+};
+
+module.exports = User;
