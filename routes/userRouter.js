6067f4d9eb5ee12ec2daefd086b6e6888dd838ca
legalvao1 <legalvao1@gmail.com>
2021-10-13 17:06:42 -0300

    Chamando a funcao no userController

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,6 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.post('/', userController);
+router.post('/', userController.createUser);
 
 module.exports = router;
