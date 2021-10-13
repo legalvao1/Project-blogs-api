e681202d82a3defdda64352bcafea4c2e3f4a473
legalvao1 <legalvao1@gmail.com>
2021-10-13 17:05:31 -0300

    Removendo atributo id

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const User = (sequelize, DataTypes) => {
   const user = sequelize.define('User', {
-    id: DataTypes.INTEGER,
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
