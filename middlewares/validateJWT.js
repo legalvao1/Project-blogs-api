09fc338b3ce9f49c3485ddce769887190e765b35
legalvao1 <legalvao1@gmail.com>
2021-10-14 00:11:50 -0300

    Middleware para validaçao do token

---
+++ middlewares/validateJWT.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const secret = process.env.JWT_SECRET;
+
+const validateJWT = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(401).json({ message: 'Token not found' });
+
+  try {
+    const payload = jwt.verify(token, secret);
+    
+    req.user = payload.data;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
+
+module.exports = validateJWT;
