bc68c953381e83d4b0556907bba6ebaea9295095
legalvao1 <legalvao1@gmail.com>
2021-10-13 23:09:08 -0300

    criando middleware para gerar token e corrigindo mensagem de erro

--- middlewares/validationMiddlewares.js
+++ middlewares/validationMiddlewares.js
@@ -1,3 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const secret = process.env.JWT_SECRET;
+
+const jwtConfiguration = {
+  expiresIn: '15m',
+  algorithm: 'HS256',
+};
+
+const generateToken = (id, email) => {
+  const payload = { id, email };
+  return jwt.sign({ data: payload }, secret, jwtConfiguration);
+};
+
 const validateName = (name) => {
   if (!name || typeof (name) !== 'string' || name.length < 8) {
     return {
@@ -12,6 +28,8 @@ const validateName = (name) => {
 
 const validateEmail = (email) => {
   const emailRegex = /\S+@\S+\.\S+/;
+  if (email === '') return { err: { status: 400, message: '"email" is not allowed to be empty' } };
+
   if (!email) return { err: { status: 400, message: '"email" is required' } };
 
   if (!emailRegex.test(email)) {
@@ -21,6 +39,10 @@ const validateEmail = (email) => {
 };
 
 const validatePassword = (password) => {
+  if (password === '') {
+    return { err: { status: 400, message: '"password" is not allowed to be empty' } };
+  }
+
   if (!password) return { err: { status: 400, message: '"password" is required' } };
 
   if (password.length !== 6) {
@@ -33,4 +55,5 @@ module.exports = {
   validateName,
   validateEmail,
   validatePassword,
+  generateToken,
 };
