3a8f5a94795c8a076324dd9c152eac6b3e6e8613
legalvao1 <legalvao1@gmail.com>
2021-10-14 15:12:44 -0300

    Desafio 6 - Controller para pegar todas as categorias

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -12,6 +12,16 @@ const createCategory = async (req, res) => {
   }
 };
 
+const getCategories = async (_req, res) => {
+  try {
+    const categories = await categoryService.getCategories();
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
   createCategory,
+  getCategories,
 };
