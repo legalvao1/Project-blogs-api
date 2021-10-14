bb2cc0a65ed031a1fa1aeadcf41cbf1b1ceaeee9
legalvao1 <legalvao1@gmail.com>
2021-10-14 00:12:28 -0300

    Removendo login controller e adicionando ao userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,18 +2,43 @@ const userService = require('../services/userService');
 
 const createUser = async (req, res) => {
   try {
-    const newUserToken = await userService.createUser(req.body);
+    const token = await userService.createUser(req.body);
 
-    if (newUserToken.err) {
-      return res.status(newUserToken.err.status).json({ message: newUserToken.err.message });
-    } 
-    return res.status(201).json({ token: newUserToken });
+    if (token.err) {
+      return res.status(token.err.status).json({ message: token.err.message });
+    }
+    res.status(201).json({ token });
   } catch (err) {
     console.log(err.message);
     res.status(500).json({ message: 'Internal server error' });
   }
 };
 
+const loginUser = async (req, res) => {
+  try {
+    const token = await userService.logUser(req.body);
+    if (token.err) {
+      return res.status(token.err.status).json({ message: token.err.message });
+    } 
+    
+    res.status(200).json({ token });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const findAll = async (_req, res) => {
+    try {
+      const users = await userService.findAll();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).send({ message: err.message });
+    }
+};
+
 module.exports = {
   createUser,
+  loginUser,
+  findAll,
 };
