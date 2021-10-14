b98931b19a9f8a6de19ed1782341daebae6ac192
legalvao1 <legalvao1@gmail.com>
2021-10-14 15:12:06 -0300

    Desafio 6 - Rota para pegar todas as categorias

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -5,5 +5,6 @@ const validateJWT = require('../middlewares/validateJWT');
 const router = express.Router();
 
 router.post('/', validateJWT, categoryController.createCategory);
+router.get('/', validateJWT, categoryController.getCategories);
 
 module.exports = router;
