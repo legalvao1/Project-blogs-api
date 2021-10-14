7e02d440b551e76b9cc01f720d66399cc3994823
legalvao1 <legalvao1@gmail.com>
2021-10-13 22:00:41 -0300

    Adicionando o express.json() para ler as req via body

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const userRouter = require('./routes/userRouter');
 
 const app = express();
-const userRouter = require('./routes/userRouter');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
