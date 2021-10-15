0c6404085ed5436eb36efb4ec380834cbb19223a
legalvao1 <legalvao1@gmail.com>
2021-10-15 16:11:55 -0300

    Desafio 10 - Configurando serve para editar um post

--- controllers/postController.js
+++ controllers/postController.js
@@ -33,8 +33,21 @@ const getPostById = async (req, res) => {
   }
 };
 
+const editPost = async (req, res) => {
+  try {
+    const post = await postService.editPost(req.params, req.body, req.user);
+    if (post.err) {
+      return res.status(post.err.status).json({ message: post.err.message });
+    }
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
   createPost,
   getPosts,
   getPostById,
+  editPost,
 };
