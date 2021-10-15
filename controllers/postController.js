370e9647f8851c6d2a160a7dc48db4591dd798ad
legalvao1 <legalvao1@gmail.com>
2021-10-15 20:44:26 -0300

    Desafio 11 - Controller para deletar um post

--- controllers/postController.js
+++ controllers/postController.js
@@ -45,9 +45,22 @@ const editPost = async (req, res) => {
   }
 };
 
+const deletePost = async (req, res) => {
+  try {
+    const post = await postService.deletePost(req.params, req.user);
+    if (post.err) {
+      return res.status(post.err.status).json({ message: post.err.message });
+    }
+    res.send(204);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
   createPost,
   getPosts,
   getPostById,
   editPost,
+  deletePost,
 };
