4d4d8b6864abfe3195c97ed7eef0de38fb6b065e
legalvao1 <legalvao1@gmail.com>
2021-10-14 21:13:12 -0300

    Desafio 7 - Confirgurando model e migration de blogPosts e criando relaçao com Users

--- models/User.js
+++ models/User.js
@@ -9,6 +9,10 @@ const User = (sequelize, DataTypes) => {
     timestamps: false,
   });
 
+  user.associate = (models) => {
+    user.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'blogPosts' });
+  };
+
   return user;
 };
 
