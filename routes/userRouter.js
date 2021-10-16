50ece7f02e464ef728a1a857c1355f41454273b3
legalvao1 <legalvao1@gmail.com>
2021-10-16 16:21:27 -0300

    Desafio 12 - Configurando rota para excluir um usuario pelo token

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/', userController.createUser);
 router.get('/', validateJWT, userController.findAll);
 router.get('/:id', validateJWT, userController.findById);
+router.delete('/me', validateJWT, userController.deleteUser);
 
 module.exports = router;
