abf5a5fee0f89cb662551260d0c5cb02b5c42e0c
legalvao1 <legalvao1@gmail.com>
2021-10-16 18:50:22 -0300

    Desafio 13 - Buscar um post pelos parametros de busca

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post('/', validateJWT, postController.createPost);
 router.get('/', validateJWT, postController.getPosts);
+router.get('/search', validateJWT, postController.getPostBySearchParams);
 router.get('/:id', validateJWT, postController.getPostById);
 router.put('/:id', validateJWT, postController.editPost);
 router.delete('/:id', validateJWT, postController.deletePost);
